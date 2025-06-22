d4e8191163879cd5911115c8f9ccd11adaa00677
Brodie <brodie.pestell7@gmail.com>
2025-06-22 17:25:14 +0100

    Implement some initial tests

--- nuget/BPITS.Results.Tests/Scaffolding/TestResultStatusCode.cs
+++ nuget/BPITS.Results.Tests/Scaffolding/TestResultStatusCode.cs
@@ -3,7 +3,16 @@ namespace BPITS.Results.Tests.Scaffolding;
 [ResultStatusCode]
 public enum TestResultStatusCode
 {
-    GenericFailure,
-    BadRequest,
-    Ok
+    GenericFailure = 0,
+    Ok = 1,
+    BadRequest = 2,
+    InsufficientPermissions = 3,
+    AuthenticationTokenInvalid = 4,
+    InvalidCredentials = 5,
+    ResourceNotFound = 6,
+    ResourceAlreadyExists = 7,
+    ResourceExpired = 8,
+    ResourceDisabled = 9,
+    FunctionalityDisabled = 10,
+    ExternalServiceFailure = 11,
 }
--- nuget/BPITS.Results.Tests/ServiceResultTests.cs
+++ nuget/BPITS.Results.Tests/ServiceResultTests.cs
@@ -1,3 +1,5 @@
+using BPITS.Results.Tests.Scaffolding;
+
 namespace BPITS.Results.Tests;
 
 public class ServiceResultTests
@@ -5,6 +7,60 @@ public class ServiceResultTests
     [Fact]
     public void ValidationFailure_ReturnsExpected()
     {
-        var result = ServiceResult.ValidationFailure("TestKey", "Test Error");    
+        var result = ServiceResult.ValidationFailure("TestKey", "Test Error");
+        Assert.Equal(TestResultStatusCode.BadRequest, result.StatusCode);
+        Assert.Equal("TestKey", result.ErrorDetails?.First().Key);
+        Assert.Contains("Test Error", result.ErrorDetails?.First().Value ?? []);
     }
+    
+    [Fact]
+    public void MapValue_WhenNotNull_ReturnsExpected()
+    {
+        var nonDtoResult = ServiceResult.Success<ExampleUser>(new ExampleUser("Test", "Test"));
+        var dtoResult = nonDtoResult.MapValue(e => e?.ToDto());
+        
+        Assert.Equal(TestResultStatusCode.Ok, dtoResult.StatusCode);
+        Assert.IsType<ExampleUserDto>(dtoResult.Value);
+        Assert.Equal("Test", dtoResult.Value?.Name);
+    }
+    
+    [Fact]
+    public void MapValue_WhenNull_ReturnsExpected()
+    {
+        var nonDtoResult = ServiceResult.Failure<ExampleUser>();
+        var dtoResult = nonDtoResult.MapValue(_ => true, _ => false);
+        
+        Assert.Equal(TestResultStatusCode.GenericFailure, dtoResult.StatusCode);
+        Assert.IsType<bool>(dtoResult.Value);
+        Assert.False(dtoResult.Value);
+    }
+    
+    [Fact]
+    public void PassthroughFail_WithNoOverride_ReturnsExpected()
+    {
+        var nonDtoResult = ServiceResult.Failure<ExampleUser>("Disabled", TestResultStatusCode.FunctionalityDisabled);
+        var passthroughResult = nonDtoResult.PassThroughFail();
+        
+        Assert.Equal(TestResultStatusCode.FunctionalityDisabled, passthroughResult.StatusCode);
+        Assert.Equal("Disabled", passthroughResult.ErrorMessage);
+    }
+    
+    [Fact]
+    public void PassthroughFail_WithOverride_ReturnsExpected()
+    {
+        var nonDtoResult = ServiceResult.Failure<ExampleUser>("Disabled", TestResultStatusCode.FunctionalityDisabled);
+        var passthroughResult = nonDtoResult.PassThroughFail("New error message", TestResultStatusCode.AuthenticationTokenInvalid);
+        
+        Assert.Equal(TestResultStatusCode.AuthenticationTokenInvalid, passthroughResult.StatusCode);
+        Assert.Equal("New error message", passthroughResult.ErrorMessage);
+    }
+}
+
+public record ExampleUser(string Name, string Password);
+
+public record ExampleUserDto(string Name);
+
+public static class ExampleUserExtensions
+{
+    public static ExampleUserDto ToDto(this ExampleUser user) => new ExampleUserDto(user.Name);
 }
